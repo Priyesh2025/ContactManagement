40febe20c9f213a11376908f41ab4786f6fee593
Priyesh2025 <tandelp000@gmail.com>
2023-10-15 15:32:49 +0530

    Setup a server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello ! I am server.");
+const express = require("express");             
+
+const dotenv = require("dotenv").config();      // for accessing contents from .env files
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+});
